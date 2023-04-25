2962fa747c0f623423aab368ac1fce3421cd1470
Soumik Bhakat <109211328+00soumik@users.noreply.github.com>
2023-04-25 23:03:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*==========toggle icon navbar============*/
+/*====================toggle icon navbar==================*/
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -10,8 +10,8 @@ menuIcon.onclick=() => {
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll=()=>{
-    sections.forEach(sec=>{
+window.onscroll = () =>  {
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset=sec.offsetTop - 150;
         let height = sec.offsetHeight;
@@ -20,10 +20,11 @@ window.onscroll=()=>{
         if(top >=offset && top < offset + height) {
             navLinks.forEach(links=>{
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*='+id+']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             })
         };
     });
+
     /*================== stickey navbar ==================*/
     let header = document.querySelector('header');
 
@@ -36,7 +37,7 @@ window.onscroll=()=>{
 
     /*================== scroll reveal ==================*/
     ScrollReveal({ 
-        //reset: true,
+        reset: true,
         distance:'80px',
         duration: 2000,
         delay:200
@@ -54,4 +55,4 @@ window.onscroll=()=>{
             backSpeed: 100,
             backDelay: 1000,
             loop: true
-         })
+         });
